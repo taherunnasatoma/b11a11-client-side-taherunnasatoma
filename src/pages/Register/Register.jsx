632789c37a1a76e866e85460993823e3d6683ae5
TaherunNasaToma <taheruntoma@gmail.com>
2025-06-17 03:13:37 +0600

    added register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,97 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import { Link } from 'react-router';
+import { AuthContext } from '../../contexts/AuthContext/AuthContext';
 
 const Register = () => {
+
+    const {createUser}=use(AuthContext)
+
+    const [error, setError] = useState('');
+
+    const handleRegister = e=>{
+        e.preventDefault()
+        const form = e.target;
+    const name = form.name.value;
+    const photoURL = form.photoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      setError(
+        'Password must contain at least one uppercase letter, one lowercase letter, and be at least 6 characters long.'
+      );
+      return;
+    }
+     console.log({ name, photoURL, email, password });
+    
+     //create user
+     createUser(email,password)
+     .then(result=>{
+        console.log(result.user)
+     })
+
+     .catch(error=>{
+        console.log(error)
+     })
+
+
+
+       
+    }
+   
+
+
+    
     return (
-        <div>
-            
+    <div className="flex justify-center min-h-screen items-center bg-gray-100">
+        
+      <div className="card bg-base-100 py-5 w-full max-w-sm shrink-0 shadow-2xl">
+        <h2 className="font-bold text-2xl text-center">Register your account</h2>
+
+        <div className="card-body">
+          <form onSubmit={handleRegister} className="space-y-4">
+            <div>
+              <label className="label">Name</label>
+              <input name="name" type="text" className="input input-bordered w-full" placeholder="Name" required />
+            </div>
+
+            <div>
+              <label className="label">Photo URL</label>
+              <input name="photoURL" type="text" className="input input-bordered w-full" placeholder="Photo URL" />
+            </div>
+
+            <div>
+              <label className="label">Email</label>
+              <input name="email" type="email" className="input input-bordered w-full" placeholder="Email" required />
+            </div>
+
+            <div>
+              <label className="label">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="input input-bordered w-full"
+                placeholder="Password"
+                required
+              />
+            </div>
+
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+
+            <button className="btn bg-[#05e9b4] text-white  w-full">Register</button>
+
+            <p className="text-center mt-4">
+              Already have an account?{' '}
+              <Link className="text-red-500 font-medium" to="/login">
+                Login
+              </Link>
+            </p>
+          </form>
         </div>
+      </div>
+    </div>
     );
 };
 
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,8 @@ import {
 } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,15 @@ const router = createBrowserRouter([
         {
             index:true,
             Component:Home
+        },
+        {
+            path:'register',
+            Component:Register
+
+        },
+        {
+            path:'login',
+            Component:Login
         }
     ]
   },
