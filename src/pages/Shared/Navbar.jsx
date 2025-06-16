1df3b2d5f97f66228c332420c86c85f693a259ad
TaherunNasaToma <taheruntoma@gmail.com>
2025-06-17 02:08:45 +0600

    design the Navbar

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
+
+    const links = <>
+     <li><NavLink to='/'>Home</NavLink></li>
+     
+    
+    </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -11,36 +18,24 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       {
+        links
+       }
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl font-bold"><span>🚗</span>
+            <h2><span className='text-[#05e9b4] text-2xl'>Dream</span>Ride Rentals</h2></a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {
+        links
+      }
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className="navbar-end gap-3">
+   <NavLink className='btn bg-[#05e9b4] text-white' to='/register'> Register</NavLink>
+   <NavLink className='btn bg-[#05e9b4] text-white' to='/login'> Login</NavLink>
   </div>
 </div>
     );
