09d772fc589c8c4e3a61c18cbeb80dd133b7d746
TaherunNasaToma <taheruntoma@gmail.com>
2025-06-17 04:48:55 +0600

    added extra section

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
 
@@ -17,6 +19,11 @@ const AuthProvider = ({children}) => {
     const signInUser=(email,password)=>{
         setLoading(true)
         return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const signInWithGoogle=()=>{
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
     }
 
     const signOutUser =()=>{
@@ -43,7 +50,8 @@ const AuthProvider = ({children}) => {
         user,
         createUser,
         signInUser,
-        signOutUser
+        signOutUser,
+        signInWithGoogle
 
     }
     return (
---
+++ src/pages/Shared/SocialLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SocialLogin = () => {
+    return (
+        <div>
+             <div className="divider">OR</div>
+              
+        </div>
+    );
+};
+
+export default SocialLogin;
